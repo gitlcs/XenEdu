31fc795ccd01b35845e8d0dcd31fda290208d76b
simc <simc@simc.be>
2013-02-21 10:18:41 +0000

    add wheezy package

---
+++ branches/wheezy/xenedu-hpraid/debian/xenedu-hpraid.default.ex
@@ -0,0 +1,10 @@
+# Defaults for xenedu-hpraid initscript
+# sourced by /etc/init.d/xenedu-hpraid
+# installed at /etc/default/xenedu-hpraid by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ branches/wheezy/xenedu-netbsdomus/debian/xenedu-netbsdomus.default.ex
@@ -0,0 +1,10 @@
+# Defaults for xenedu-netbsdomus initscript
+# sourced by /etc/init.d/xenedu-netbsdomus
+# installed at /etc/default/xenedu-netbsdomus by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
